5992f2674c074b10c006c0af5cf386ae0deb3b92
mac <gaiyulong@gmail.com>
2021-06-21 18:47:14 +0800

    feat(注入全局样式): 注入全局样式
    注入scss到vite config中，作为全局样式控制

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
   })
 </script>
 
-<style>
+<style lang="scss" scope>
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const path = require('path')
 
-// https://vitejs.dev/config/
+// path组合
+const resolvePath = dir => path.resolve(__dirname, dir)
+
 export default defineConfig({
-  plugins: [vue()]
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "@styles/index.scss";
+        `
+      }
+    }
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolvePath('src'),
+      '@styles': resolvePath('src/assets/styles')
+    }
+  }
 })
